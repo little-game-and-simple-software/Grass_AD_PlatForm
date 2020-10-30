b790bf88a7c8a86e7135d8a605d922c34796d71e
2439905184 <suchenglong018@126.com>
2020-10-30 19:29:14 +0800

    添加了一些广告图片 随机数生成 随机生成广告

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
+//console.log("欢迎来到草根广告联盟")
 function Error()
 {
 	alert("暂时未开放！")
@@ -9,4 +10,26 @@ function Error1()
 function upLoadAd()
 {
 	alert("如果需要投放广告，请将广告资料和广告图提交到2439905184@qq.com等待站长审核之后即可投放")
+}
+//随机展示广告图片测试
+
+//打开新的广告页面
+function openNewAdView()
+{
+	window.open("ShowAd.html")
+}
+function RandomAdShowTest()
+{/*
+	//alert("开始测试")
+	//默认随机生成0-1之间的数 *10之后范围变成0-10
+	//floor方法是数学中的去尾法 去除小数结尾的数字 保留整数
+	//从Main.js生成随机数 需要将随机数存入全局变量
+	//打开新的广告页面时 加载完成 需要获得生成的随机数
+	//根据随机数来选择如何显示对应的广告
+	var FirstRandomNumber=Math.random()
+	var ResultNumber=Math.floor(FirstRandomNumber)
+	console.log(ResultNumber)
+	alert(ResultNumber)
+	console.log("测试随机数")
+	window.open("ShowAd.html")*/
 }
---
+++ ShowAd.js
@@ -0,0 +1,32 @@
+//初始化获得广告图像 需要优先运行
+function getAdImageNode(){
+
+ImageAd=document.getElementById("Ad")
+
+RandomAdShowTest()
+}
+function RandomAdShowTest()
+{
+	//默认随机生成0-1之间的数 *10之后范围变成0-10
+	//floor方法是数学中的去尾法 去除小数结尾的数字 保留整数
+	//从Main.js生成随机数 需要将随机数存入全局变量
+	//打开新的广告页面时 加载完成 需要获得生成的随机数
+	//根据随机数来选择如何显示对应的广告
+	var FirstRandomNumber=Math.random()*4
+	var ResultNumber=Math.floor(FirstRandomNumber)
+	console.log(ResultNumber)
+	console.log("测试随机数")
+	if(ResultNumber==1)
+	{
+	ImageAd.src="Ads/小沙盒工作室.jpg"
+	}
+	else if(ResultNumber==2)
+	{
+	ImageAd.src="Ads/啤酒广告.webp"
+	}
+	else if(ResultNumber==3)
+	{
+	ImageAd.src="Ads/蛋糕广告.webp"
+	}
+}
+//getAdImageNode()
